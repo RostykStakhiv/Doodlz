0021a5c928e9299ea5e93919ad296f231516c26a
Oleh Onyshchak <oleh.onyshchak@gmail.com>
2017-11-09 13:05:52 +0200

    Added additional view dependecies

---
+++ Doodlz/app/src/main/java/com/pmi/doodlz/GeneralViewDialog.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.net.Uri;
+import android.provider.MediaStore.Images;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+// the main screen that is painted
+public class DoodleView extends View 
+{
+	
+};
